f6743ab8228f4be55c9ba1ad536e29b3eae6e980
aml-fakhry <amlfakhry13@email.com>
2025-04-08 13:06:19 +0200

    Set up basic Express server with request parsing and route registration.

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,57 @@
-
 import express from 'express';
+import dotenv from 'dotenv';
 
+
+dotenv.config();
+const PORT = process.env.PORT;
 const app = express();
-const port = 3000;
 
-app.get('/', (_req, res) => {
-  res.send('Hello from TypeScript + Express!');
-});
+/**
+ * Sets the http-request options for an express server.
+ * @param app The express application to set its express server's request options.
+ */
+function setRequestOptions(app:any) {
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+  /**
+   * Allow parse incoming requests as JSON payloads.
+   */
+  app.use(express.json({ limit: '5mb' }));
+
+  /**
+   * Allow parse incoming urlencoded requests bodies.
+   */
+  app.use(express.urlencoded({ limit: '5mb', extended: true }));
+}
+
+function registerRoutes(app:any) {
+  /**
+   * The base-route prefix for the api.
+   *
+   * e.g. `/api/organizations`, `/api/products`.
+   */
+  const apiBaseRoute = '/api/';
+
+}
+
+function setupServer(app:any) {
+  /**
+   * The order matters.
+   * 2. Set request options.
+   * 3. Register routes.
+   */
+
+  setRequestOptions(app);
+  registerRoutes(app);
+}
+
+/**
+ * Starts an express server.
+ */
+async function startServer() {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+}
+
+setupServer(app);
+startServer();
