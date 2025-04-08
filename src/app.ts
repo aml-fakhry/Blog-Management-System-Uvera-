3dee326cafcaeaeb304fb8b88a281c305371124c
aml-fakhry <amlfakhry13@email.com>
2025-04-08 12:00:55 +0200

    setup the project.

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (_req, res) => {
+  res.send('Hello from TypeScript + Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
